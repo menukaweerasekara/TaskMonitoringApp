09ce5a311943529362759629fa44335c8027379f
Menuka Weerasekara <ac106761@avcol.school.nz>
2021-06-17 12:28:25 +1200

    Add project files.

---
+++ TaskMonitoringApp/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskMonitoringApp
+{
+    class Task
+    {
+        public Task (string taskname, string duedate)
+        {
+            TaskName = taskname;
+            Duedate = duedate;
+        }
+        public string TaskName { get; set; }
+        public string Duedate { get; set; }
+    }
+}
---
+++ TaskMonitoringApp/TaskList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace TaskMonitoringApp
+{
+    class TaskList
+    {
+        private List<Task> _tasks { get; set; }
+
+        private void DisplayTaskDetails(Task task)
+        {
+            Console.WriteLine($"Task: {task.TaskName}, {task.Duedate}");
+        }
+
+        private void DisplayTasksDetails(List <Task> tasks)
+        {
+            foreach (var task in tasks)
+            {
+                DisplayTaskDetails(task);
+
+            }
+
+        }
+
+        public void AddTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+
+        public void DisplayTask(string duedate)
+        {
+            var task = _tasks.FirstOrDefault(t => t.Duedate == duedate);
+            if (task == null)
+            {
+                Console.WriteLine("Task not found");
+            }
+            else
+            {
+                DisplayTaskDetails(task);
+            }
+            
+        }
+
+        public void DisplayAllTask()
+        {
+            DisplayTasksDetails(_tasks);
+        }
+
+        public void DisplayMatchingTasks(string searchPhrase)
+        {
+            var matchingTasks = _tasks.Where(t => t.TaskName.Contains(searchPhrase)).ToList();
+            DisplayTasksDetails(matchingTasks);
+        }
+
+    }
+
+}
